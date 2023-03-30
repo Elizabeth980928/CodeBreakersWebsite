28b7036d9c64d493bae5d12b9fa76a02e198db62
Mandisa222 <mandyngabaza@gmail.com>
2023-03-30 16:06:26 +0200

    Ignore

--- staff.js
+++ staff.js
@@ -3,3 +3,10 @@ function topScroll(){
   window.scrollTo({top:0, behavior: "smooth"});
 }
 
+function imageGrow() {
+  document.getElementsById("section1").style.width = "150px";
+}
+function imageShrink() {
+  document.getElementsById("section1").style.width = "100px";
+}
+

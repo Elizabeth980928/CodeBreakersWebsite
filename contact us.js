a0f99501b0151910a74f1a6b173678f43a486a02
AsisiphoMaliwa <asisiphomaliwa3@gmail.com>
2023-03-30 23:01:44 +0200

    contact with nav js

--- contact us.js
+++ contact us.js
@@ -1,3 +1,37 @@
+// nav bar js
+function openlink(evt, linkName) {
+  // Declare all variables
+  var i, tabcontent,  tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTab");
+  if (x.className === "tab") {
+    x.className += " responsive";
+  } else {
+    x.className = "tab";
+  }
+}
+
+
+// form validation
 var validate = function(e) {
     var fields = document.querySelectorAll('.form-container textarea, .form-container input[type="text"]');
     var regEx;
